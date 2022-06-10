d803e8a292fb45f617762bc2b90e4f839f689de0
cxu <11200229@qq.com>
2022-06-10 18:00:23 +0800

    added py

--- test.py
+++ test.py
@@ -7,12 +7,16 @@ lg = bs.login()
 print('login respond error_code:'+lg.error_code)
 print('login respond  error_msg:'+lg.error_msg)
 
+day = "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST"
+
+week = "date,code,open,high,low,close,volume,amount,adjustflag,turn,pctChg"
+
 #### 获取历史K线数据 ####
 # 详细指标参数，参见“历史行情指标参数”章节
-rs = bs.query_history_k_data_plus("sh.600000",
-    "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST",
-    start_date='2017-06-01', end_date='2017-12-31', 
-    frequency="d", adjustflag="3") #frequency="d"取日k线，adjustflag="3"默认不复权
+rs = bs.query_history_k_data_plus("sz.002284",
+    week,
+    start_date='2017-06-01', end_date='2022-06-10',
+    frequency="w", adjustflag="2") #frequency="d"取日k线，adjustflag="3"默认不复权
 print('query_history_k_data_plus respond error_code:'+rs.error_code)
 print('query_history_k_data_plus respond  error_msg:'+rs.error_msg)
 
@@ -23,7 +27,7 @@ while (rs.error_code == '0') & rs.next():
     data_list.append(rs.get_row_data())
 result = pd.DataFrame(data_list, columns=rs.fields)
 #### 结果集输出到csv文件 ####
-result.to_csv("D:/history_k_data.csv", encoding="gbk", index=False)
+result.to_csv("~/002284.w.history_k_data.csv", encoding="gbk", index=False)
 print(result)
 
 #### 登出系统 ####
