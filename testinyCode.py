2b780eb883181069a710223e33dc77047f1818f3
cxu <11200229@qq.com>
2022-08-11 17:28:22 +0800

    longterm export

--- testinyCode.py
+++ testinyCode.py
@@ -20,7 +20,24 @@ print(c)
 # todayStr = today.strftime("%Y-%m-%d")
 #
 # startDateStr = (date.today() + timedelta(weeks=-500)).strftime("%Y-%m-%d")
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+print(f"{bcolors.FAIL}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
+
+a = 1
+b = 2
+
+# print("fjdsfjs jdfslf  %a is %b" % {'a': a, 'b': b})
+# print("fjdsfjs jdfslf  ", a , sep=", ")
 
 today = date.today().strftime("%Y%m%d")
 startDate = (date.today() + relativedelta(months=-8)).strftime("%Y%m%d")
