497faf023156dfc63394439f8220f360fea41f09
cxu <11200229@qq.com>
2022-08-22 10:01:10 +0800

    longterm

--- excelTesting.py
+++ excelTesting.py
@@ -1,8 +1,8 @@
 from xlwt import Workbook
 import xlwt
-book = Workbook()
+book = Workbook(style_compression=10)
 sheet1 = book.add_sheet('Sheet 1')
-for i in range(0, 100000):
+for i in range(0, 5000):
     st = xlwt.easyxf('pattern: pattern solid;')
     st.pattern.pattern_fore_colour = i
     sheet1.write(i % 24, i // 24, 'Test text', st)
