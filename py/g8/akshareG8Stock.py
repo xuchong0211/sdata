035cbb84ab7d3f515f17b7dce70830080359eb40
cxu <11200229@qq.com>
2022-09-02 15:34:53 +0800

    implement g8 thread

--- py/g8/akshareG8Stock.py
+++ py/g8/akshareG8Stock.py
@@ -4,6 +4,166 @@ import pandas as pd
 from datetime import date, timedelta
 import numpy as np
 
+import threading
+
+
+
+
+def calcMA(arr, window_size):
+    # Program to calculate moving average
+
+    i = 0
+    # Initialize an empty list to store moving averages
+    moving_averages = []
+
+    # Loop through the array to consider
+    # every window of size 3
+    while i < len(arr) - window_size + 1:
+
+        # Store elements from i to i+window_size
+        # in list to get the current window
+        window = arr[i : i + window_size]
+
+        # Calculate the average of current window
+        window_average = round(sum(window) / window_size, 2)
+
+        # Store the average of current
+        # window in moving average list
+        moving_averages.append(window_average)
+
+        # Shift window to right by one position
+        i += 1
+
+    print(moving_averages)
+    return moving_averages
+
+
+def calcMAEligible(arr, window_size):
+    # Program to calculate moving average
+    eligible = True
+    value = 0
+
+    i = 0
+    # Initialize an empty list to store moving averages
+    moving_averages = []
+
+    # Loop through the array to consider
+    # every window of size 3
+    while i < len(arr) - window_size + 1:
+
+        # Store elements from i to i+window_size
+        # in list to get the current window
+        window = arr[i : i + window_size]
+
+        # Calculate the average of current window
+        window_average = round(sum(window) / window_size, 2)
+
+        if value==0 :
+            value = window_average
+        else :
+            eligible = eligible and value >= window_average
+        value = window_average
+
+        # if eligible:
+        #   eligible = arr[i] >= window_average
+
+        # Store the average of current
+        # window in moving average list
+        moving_averages.append(window_average)
+
+        # Shift window to right by one position
+        i += 1
+
+    print(moving_averages)
+
+    return {"ma": moving_averages, "eligible":eligible}
+
+
+
+#
+#
+#
+
+count=0
+
+def saveStock(list):
+
+    for stock in list:
+
+        code=stock['code']
+        name=stock['name']
+
+        print("start to save stock ================================================" , code , name, end='\n * ')
+
+        stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code, period=period, start_date=startDate, end_date=today, adjust="qfq")
+        # print(stock_zh_a_hist_df)
+        global count
+        count = count + 1
+        print("count:  " , count, end='\n')
+        data=[]
+        closeList=[]
+        #
+        for index, row in stock_zh_a_hist_df.iterrows():
+            #     result = db.save(row)
+
+            # 日期	object	交易日 0
+            # 开盘	float64	开盘价 1
+            # 收盘	float64	收盘价 2
+            # 最高	float64	最高价 3
+            # 最低	float64	最低价 4
+            # 成交量	int32	注意单位: 手 5
+            # 成交额	float64	注意单位: 元 6
+            # 振幅	float64	注意单位: % 7
+            # 涨跌幅	float64	注意单位: % 8
+            # 涨跌额	float64	注意单位: 元 9
+            # 换手率	float64	注意单位: % 10
+            data.append({
+                'name': name,
+                'code': code,
+                'date': row[0],
+                'open': row[1],
+                'close': row[2],
+                'high': row[3],
+                'low': row[4],
+                'volume': row[5],
+                'turn': row[6],
+                'zhenfu': row[7],
+                'range': row[8],
+                'amount': row[9],
+                'turnover': row[10],
+            })
+            closeList.append(row[2])
+
+        if len(data) > 0 :
+            data.reverse()
+            closeList.reverse()
+
+        ma10Data = calcMAEligible(closeList, 10)
+        ma60Data = calcMAEligible(closeList, 60)
+
+        ma10 = ma10Data["ma"][0 : 20]
+        ma60 = ma60Data["ma"][0 : 20]
+
+        ma10Up = ma10Data["eligible"]
+        ma60Up = ma60Data["eligible"]
+
+
+        data = data[0 : 50]
+
+        row = {
+            '_id': data[0]["date"] +"_"+code,
+            'date': data[0]["date"],
+            'name': name,
+            'code': code,
+            'weeks': data,
+            'ma10': ma10,
+            'ma60': ma60,
+            'ma10Up': ma10Up,
+            'ma60Up': ma60Up,
+        }
+
+        db.save(row)
+
 
 
 today = date.today().strftime("%Y%m%d")
@@ -26,169 +186,83 @@ design_view = {
 couch = couchdb.Server('http://admin:password@127.0.0.1:5984/')
 db = couch.create('g8_'+today)
 # db = couch['g8_']
-stocks = ak.stock_zh_a_spot_em()
 
 
 # period="monthly"
 # period="daily"
 period="weekly"
 
+group = 200
 
-count=0
+if True :
+    stockList = []
 
-for index, srow in stocks.iterrows():
+    stocks = ak.stock_zh_a_spot_em()
+    for sindex, srow in stocks.iterrows():
 
-# 名称	类型	描述
-# 序号	int64	-
-# 代码	object	-
-# 名称	object	-
-# 最新价	float64	-
-# 涨跌幅	float64	注意单位: %
-# 涨跌额	float64	-
-# 成交量	float64	注意单位: 手
-# 成交额	float64	注意单位: 元
-# 振幅	float64	注意单位: %
-# 最高	float64	-
-# 最低	float64	-
-# 今开	float64	-
-# 昨收	float64	-
-# 量比	float64	-
-# 换手率	float64	注意单位: %
-# 市盈率-动态	float64	-
-# 市净率	float64	-
-# 总市值	float64	注意单位: 元
-# 流通市值	float64	注意单位: 元
-# 涨速	float64	-
-# 5分钟涨跌	float64	注意单位: %
-# 60日涨跌幅	float64	注意单位: %
-# 年初至今涨跌幅	float64	注意单位: %
-    code = srow[1]
-    name = srow[2]
-    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code, period=period, start_date=startDate, end_date=today, adjust="qfq")
-    # print(stock_zh_a_hist_df)
+    # 名称	类型	描述
+    # 序号	int64	-
+    # 代码	object	-
+    # 名称	object	-
+    # 最新价	float64	-
+    # 涨跌幅	float64	注意单位: %
+    # 涨跌额	float64	-
+    # 成交量	float64	注意单位: 手
+    # 成交额	float64	注意单位: 元
+    # 振幅	float64	注意单位: %
+    # 最高	float64	-
+    # 最低	float64	-
+    # 今开	float64	-
+    # 昨收	float64	-
+    # 量比	float64	-
+    # 换手率	float64	注意单位: %
+    # 市盈率-动态	float64	-
+    # 市净率	float64	-
+    # 总市值	float64	注意单位: 元
+    # 流通市值	float64	注意单位: 元
+    # 涨速	float64	-
+    # 5分钟涨跌	float64	注意单位: %
+    # 60日涨跌幅	float64	注意单位: %
+    # 年初至今涨跌幅	float64	注意单位: %
+        code = srow[1]
+        name = srow[2]
 
-    print("start.........: "+name)
-    count = count + 1
-    data=[]
-    #
-    for index, row in stock_zh_a_hist_df.iterrows():
-        #     result = db.save(row)
 
-    # 日期	object	交易日 0
-    # 开盘	float64	开盘价 1
-    # 收盘	float64	收盘价 2
-    # 最高	float64	最高价 3
-    # 最低	float64	最低价 4
-    # 成交量	int32	注意单位: 手 5
-    # 成交额	float64	注意单位: 元 6
-    # 振幅	float64	注意单位: % 7
-    # 涨跌幅	float64	注意单位: % 8
-    # 涨跌额	float64	注意单位: 元 9
-    # 换手率	float64	注意单位: % 10
-        data.append({
+        stockList.append({
             'name': name,
-            'code': code,
-            'date': row[0],
-            'open': row[1],
-            'close': row[2],
-            'high': row[3],
-            'low': row[4],
-            'volume': row[5],
-            'turn': row[6],
-            'zhenfu': row[7],
-            'range': row[8],
-            'amount': row[9],
-            'turnover': row[10],
-            })
-        # print("count : " + str(count))
-    # db.save({'_id':  today + '_' + code,
-    #             'date': today,
-    #             'name': name,
-    #             'code': code,
-    #             'data': data,
-    #             })
-
-    print("==================data======================="+str(len(data)))
-    # print(data)
+            'code': code
+        })
 
 
-    if len(data) > 0 :
+        if len(stockList) >= group:
+            t1 = threading.Thread(target=saveStock, args=(stockList,))
+            t1.start()
+            print("start thread =====================> " , sindex, end='\n')
+            stockList=[]
 
-        data.reverse()
-        ma10_0 = 0
-        ma10_1 = 0
-        ma10_2 = 0
-        ma10_3 = 0
+    if len(stockList) > 0:
+        t1 = threading.Thread(target=saveStock, args=(stockList,))
+        t1.start()
+        print("start thread =====================> last", end='\n')
+        stockList=[]
 
-        ma60_0 = 0
-        ma60_1 = 0
-        ma60_2 = 0
-        ma60_3 = 0
-
-        weeks = []
-
-        for i in range(len(data)) :
-
-            if i < 30 :
-                weeks.append(data[i])
-
-            # print("index=============================="+ str(i))
-            if i < 60 :
-                ma60_0 = ma60_0 + data[i]["close"]
+else :
+    print("222222222222222222222222222222222", end='\n')
+    saveStock([{
+        'name': "中路股份",
+        'code': "600818"
+    }])
 
 
-            if i > 0 and i < 61 :
-                ma60_1 = ma60_1 + data[i]["close"]
 
 
-            if i > 1 and i < 62 :
-                ma60_2 = ma60_2 + data[i]["close"]
 
 
-            if i > 2 and i < 63 :
-                ma60_3 = ma60_3 + data[i]["close"]
-
-            # print("index=============================="+ str(i))
-
-            if i < 10 :
-                ma10_0 = ma10_0 + data[i]["close"]
 
 
-            if i > 0 and i < 11 :
-                ma10_1 = ma10_1 + data[i]["close"]
 
 
-            if i > 1 and i < 12 :
-                ma10_2 = ma10_2 + data[i]["close"]
 
-
-            if i > 2 and i < 13 :
-                ma10_3 = ma10_3 + data[i]["close"]
-
-
-        print("==========10============")
-        print(round((ma10_0/10),3))
-        print(round((ma10_1/10),3))
-        print(round((ma10_2/10),3))
-        print(round((ma10_3/10),3))
-
-        print("==========60============")
-        print(round((ma60_0/60),3))
-        print(round((ma60_1/60),3))
-        print(round((ma60_2/60),3))
-        print(round((ma60_3/60),3))
-
-        row = {
-            '_id': weeks[0]["date"] +"_"+code,
-            'date': weeks[0]["date"],
-            'name': name,
-            'code': code,
-            'weeks': weeks,
-            'm10': [round((ma10_0/10),3),round((ma10_1/10),3),round((ma10_2/10),3),round((ma10_3/10),3)],
-            'm60': [round((ma60_0/60),3),round((ma60_1/60),3),round((ma60_2/60),3),round((ma60_3/60),3)],
-        }
-
-        db.save(row)
 print("............start analysis.............")
 
 db.save(design_view)
